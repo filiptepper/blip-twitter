3483feececc1a2503ea86771ecb1baca8101bfd7
Filip Tepper <filip@tepper.pl>
2009-12-29 15:41:55 +0100

    fixed dependencies

--- blip-twitter-api.rb
+++ blip-twitter-api.rb
@@ -1,6 +1,5 @@
 require "rubygems"
 require "sinatra"
-require "httparty"
 require "vendor/gems/httparty-0.5.0/lib/httparty"
 require "vendor/gems/bliptwitt-0.0.3/lib/bliptwitt"
 require "vendor/gems/yajl-ruby-0.6.7/lib/yajl"
